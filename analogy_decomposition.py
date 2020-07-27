35aa0d519a7524d7f079f710db2545b5bed2c90b
Louis Fournier <louis.fournier@gleamer.ai>
2020-07-27 20:16:11 +0200

    Fixing pandas?

--- analogy_decomposition.py
+++ analogy_decomposition.py
@@ -136,7 +136,10 @@ def save_decompo(names, results, decomposition):
     if decomposition == 'decomposition_ref': columns = np.array(['Categories', 'b*analogy', 'o_a*b', 'o_a^2'])
     if decomposition == 'delta_sim': columns = np.array(['Categories', 'b*analogy', 'o_a*o_b', 'o_a*b'])
     print(len(names), len(results[0]), len(results[1]), len(results[2]))
-    df = pd.DataFrame(np.array([[names,results[0],results[1],results[2]]]), columns=columns)
+    r = np.array([names,results[0],results[1],results[2]])
+    print(r.shape)
+    print(columns.shape)
+    df = pd.DataFrame(np.array([names,results[0],results[1],results[2]]), columns=columns)
 
     timestr = time.strftime("%Y%m%d-%H%M%S")
     namepath = str(decomposition) + '-' + str(timestr) + '.csv'
