4dc843d9a0e5c670c195a4cda4368d9df21a1331
Louis Fournier <louis.fournier@gleamer.ai>
2020-07-31 09:10:52 +0200

    Fix length

--- random_sets.py
+++ random_sets.py
@@ -477,12 +477,13 @@ def metrics_random_from_model(model, nb_perms=50, nb_random=10, size_random_cate
     similarities_shuffle = [similarite_offsets(np.array(shf_offsets)[:, perm])
                             for perm in range(nb_perms)]
 
+    similarities_random_results = similarities_random(offsets_random, pairs_sets, vocabulary, nb_random=10)
     similarities_random_shuffle = similarities_shuffle_random(offsets_random_shuffle,
                                                               nb_random=nb_random, nb_perms=nb_perms)
 
     ocs_all, pcs_all = ocs_pcs_random(similarities,
                                       similarities_shuffle,
-                                      similarities_random,
+                                      similarities_random_results,
                                       similarities_random_shuffle,
                                       nb_random=nb_random, nb_perms=nb_perms)
 
